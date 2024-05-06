d1f71d7f9c9990def92bac9b4871f1981d2513a5
ErwanR-Holberton <6218@holbertonstudents.com>
2024-05-06 15:51:20 +0200

    Withour recursion

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -4,24 +4,21 @@ solves lockboxes problem
 """
 
 
-def open_box(index, boxes, opened_boxes):
-    """open boxes recursively"""
-    if index >= len(boxes):
-        return False
-    opened_boxes.append(index)
-    for key in boxes[index]:
-        if key not in opened_boxes:
-            if open_box(key, boxes, opened_boxes):
-                return True
-    if len(boxes) == len(opened_boxes):
-        return True
-    return False
-
-
 def canUnlockAll(boxes):
     """main function called used to call the other one"""
     opened_boxes = []
-    if open_box(0, boxes, opened_boxes):
+    keys = [0]
+
+    while len(keys) > 0:
+        if keys[0] not in opened_boxes and keys[0] < len(boxes):
+            opened_boxes.append(keys[0])
+            for key_in_box in boxes[keys[0]]:
+                if key_in_box not in opened_boxes and key_in_box not in keys and key_in_box < len(boxes):
+                    keys.append(key_in_box)
+
+        del keys[0]
+
+    if len(boxes) == len(opened_boxes):
         return True
 
     return False
