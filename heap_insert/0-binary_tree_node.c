5f4a4e8a84d232dd6ef9917f61ea817cf44a7b19
ErwanR-Holberton <6218@holbertonstudents.com>
2024-05-21 16:04:53 +0200

    Added betty doc

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -2,16 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/**
+ * binary_tree_node - Entry point
+ *
+ * @parent: Pointer to the parent
+ * @value: value of the node
+ * 
+ * Return: Always 0 (Success)
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    
-	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+    binary_tree_t *new = malloc(sizeof(binary_tree_t));
+
 	if (new == NULL)
 		return (NULL);
     new->n = value;
     new->parent = parent;
     new->left = NULL;
     new->right = NULL;
-    
+
     return new;
 }
