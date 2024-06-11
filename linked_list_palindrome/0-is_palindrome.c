46f245ac90c1bdf95311b22c0bf4e2f810fee1a6
ErwanR-Holberton <6218@holbertonstudents.com>
2024-06-11 09:03:06 +0200

    two pointers method

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -30,25 +30,23 @@ int len(listint_t **head)
  * @index: index
  * Return: value at index
  */
-int get(listint_t **head, int index)
+listint_t *get(listint_t *head, int index)
 {
 	listint_t *current;
 	int count = 0;
 
-	current = *head;
+	current = head;
 
-	if (*head == NULL)
-		return (-1);
 	while (count < index)
 	{
 		if (current == NULL)
-			return (-1);
+			return (NULL);
 		current = current->next;
 		count++;
 	}
 	if (current == NULL)
-		return (-1);
-	return (current->n);
+		return (NULL);
+	return (current);
 }
 
 /**
@@ -58,22 +56,18 @@ int get(listint_t **head, int index)
  */
 int is_palindrome(listint_t **head)
 {
-	int min = 0, max = -1;
-	int arr[2000];
+	int min = 0, lenght = len(head), max;
+	listint_t *current = *head, *mid = get(*head, lenght / 2);
 
-	while (*head != NULL)
-	{
-		arr[max + 1] = (*head)->n;
-		*head = (*head)->next;
-		max++;
-	}
+	max = lenght - 1;
 
 	while (max > min)
 	{
-		if (arr[min] != arr[max])
-			return (0);
+		if (current->n != get(mid, lenght - (lenght / 2) - 1 - min)->n)
+			return 0;
+		current = current->next;
 		min++;
-		max--;
+		max --;
 	}
 	return (1);
 }
