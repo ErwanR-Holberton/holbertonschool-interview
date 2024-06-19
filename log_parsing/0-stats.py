8b9ee6304c8e84c364e92578ef184c77e16b50a3
ErwanR-Holberton <6218@holbertonstudents.com>
2024-06-19 11:30:13 +0200

    Task 1

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -2,8 +2,25 @@
 import sys
 
 input_data = sys.stdin.read()
+lines = input_data.splitlines()
 
-if input_data == "":
-    print("noooooooooooooooooooooooo")
+count = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+total_size = 0
 
-print("sfdqs    ", input_data)
+for line in lines:
+
+    slip = line.split(" ")
+    total_size += int(slip[8])
+
+    if int(slip[7]) in status_codes:
+        status_codes[int(slip[7])] += 1
+
+    count += 1
+
+    if count %10 == 0:
+        print("File size:", total_size)
+        for code, amount in status_codes.items():
+            print(code, end="")
+            print(":", amount)
+
