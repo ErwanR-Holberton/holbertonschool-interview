2ffa107e31a5ae2ae68d5992a55e1ba3bb0b9e84
ErwanR-Holberton <6218@holbertonstudents.com>
2024-06-24 14:35:36 +0200

    Use time

--- linked_list_cycle/0-check_cycle.c
+++ linked_list_cycle/0-check_cycle.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <time.h>
 
 /**
  * check_cycle - test if list has a loop
@@ -7,13 +8,20 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *copy = list;
+	listint_t *copy = list, *copy2 = NULL;
+	time_t startTime = time(NULL);
+	int pause = 0;
 
-	while (copy->next != NULL)
+	while (copy != NULL)
 	{
 		copy = copy->next;
-		if (copy == list)
+		if (copy == list || (copy == copy2 && copy2 != NULL))
 			return (1);
+		if (!pause && time(NULL) - 1 > startTime)
+		{
+			pause = 1;
+			copy2 = copy;
+		}
 	}
 	return (0);
 }
