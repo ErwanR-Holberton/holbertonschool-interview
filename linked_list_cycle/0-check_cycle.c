90bddf61e9179a046204f455fe4045b2c7431f50
ErwanR-Holberton <6218@holbertonstudents.com>
2024-06-24 14:45:11 +0200

    pointers methods

--- linked_list_cycle/0-check_cycle.c
+++ linked_list_cycle/0-check_cycle.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <time.h>
 
 /**
  * check_cycle - test if list has a loop
@@ -8,20 +7,16 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *copy = list, *copy2 = NULL;
-	time_t startTime = time(NULL);
-	int pause = 0;
+	listint_t *copy = list, *copy2 = list;
 
 	while (copy != NULL)
 	{
 		copy = copy->next;
-		if (copy == list || (copy == copy2 && copy2 != NULL))
+		copy2 = copy2->next;
+		if (copy != NULL)
+			copy = copy->next;
+		if (copy == copy2)
 			return (1);
-		if (!pause && time(NULL) - 1 > startTime)
-		{
-			pause = 1;
-			copy2 = copy;
-		}
 	}
 	return (0);
 }
