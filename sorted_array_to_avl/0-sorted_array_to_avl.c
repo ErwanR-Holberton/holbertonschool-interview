c5e3b81433f3b268b3409ec5545a7aa4469711a6
ErwanR-Holberton <6218@holbertonstudents.com>
2024-08-05 12:32:08 +0200

    Push files task not done

---
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,11 @@
+#include  "binary_trees.h"
+
+
+
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	int left = size / 2, right = size / 2;
+	if (array || right || left)
+		return NULL;
+	return NULL;
+}
