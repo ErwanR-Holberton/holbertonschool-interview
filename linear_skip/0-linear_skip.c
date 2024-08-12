d8236a38763faae855587b4d042ad19487f89f51
ErwanR-Holberton <6218@holbertonstudents.com>
2024-08-12 15:14:55 +0200

    Task 0

---
+++ linear_skip/0-linear_skip.c
@@ -0,0 +1,42 @@
+#include "search.h"
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *current = list, *current2;
+	while (current->express != NULL)
+	{
+		printf("Value checked at index [%lld] = [%d]\n", current->express->index, current->express->n);
+		if (current->express->n < value)
+			current = current->express;
+		else
+		{
+			printf("Value found between indexes [%lld] and [%lld]\n", current->index, current->express->index);
+			while (current != NULL)
+			{
+				printf("Value checked at index [%lld] = [%d]\n", current->index, current->n);
+				if (current->n < value)
+					current = current->next;
+				else if (current->n == value)
+					return current;
+				else
+					return NULL;
+			}
+			break;
+		}
+	}
+	current2 = current;
+	while (current2->next!= NULL)
+		current2 = current2->next;
+	printf("Value found between indexes [%lld] and [%lld]\n", current->index, current2->index);
+	while (current != NULL)
+	{
+		printf("Value checked at index [%lld] = [%d]\n", current->index, current->n);
+		if (current->n < value)
+			current = current->next;
+		else if (current->n == value)
+			return current;
+		else
+			return NULL;
+	}
+	return NULL;
+}
