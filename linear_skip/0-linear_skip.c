32e30d3a7730c26159fdafcd431b3758a450d1e8
ErwanR-Holberton <6218@holbertonstudents.com>
2024-08-12 16:15:22 +0200

    Betty style

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -1,42 +1,59 @@
 #include "search.h"
 
-skiplist_t *linear_skip(skiplist_t *list, int value)
+/**
+ * search - search one by one
+ *
+ * @list: list of nodes
+ * @index2: value for the print
+ * @value: value to search
+ *
+ * Return: null or node with the value
+ */
+skiplist_t *search(skiplist_t *list, int index2, int value)
 {
-	skiplist_t *current = list, *current2;
-	while (current->express != NULL)
-	{
-		printf("Value checked at index [%zu] = [%d]\n", current->express->index, current->express->n);
-		if (current->express->n < value)
-			current = current->express;
-		else
-		{
-			printf("Value found between indexes [%zu] and [%zu]\n", current->index, current->express->index);
-			while (current != NULL)
-			{
-				printf("Value checked at index [%zu] = [%d]\n", current->index, current->n);
-				if (current->n < value)
-					current = current->next;
-				else if (current->n == value)
-					return current;
-				else
-					return NULL;
-			}
-			break;
-		}
-	}
-	current2 = current;
-	while (current2->next!= NULL)
-		current2 = current2->next;
-	printf("Value found between indexes [%zu] and [%zu]\n", current->index, current2->index);
+	skiplist_t *current = list;
+
+	printf("Value found between indexes [%zu] and [%d]\n",
+											current->index, index2);
 	while (current != NULL)
 	{
-		printf("Value checked at index [%zu] = [%d]\n", current->index, current->n);
+		printf("Value checked at index [%zu] = [%d]\n",
+												current->index, current->n);
 		if (current->n < value)
 			current = current->next;
 		else if (current->n == value)
-			return current;
+			return (current);
 		else
-			return NULL;
+			return (NULL);
 	}
-	return NULL;
+	return (NULL);
+}
+
+/**
+ * linear_skip - use linear skip method to search
+ *
+ * @list: list of nodes
+ * @value: value to search
+ *
+ * Return: null or node with the value
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *current = list, *current2;
+
+	if (list == NULL)
+		return (NULL);
+	while (current->express != NULL)
+	{
+		printf("Value checked at index [%zu] = [%d]\n",
+				current->express->index, current->express->n);
+		if (current->express->n < value)
+			current = current->express;
+		else
+			return (search(current, current->express->n, value));
+	}
+	current2 = current;
+	while (current2->next != NULL)
+		current2 = current2->next;
+	return (search(current, current2->n, value));
 }
