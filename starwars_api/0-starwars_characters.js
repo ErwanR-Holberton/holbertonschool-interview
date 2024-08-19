8d73c2f84b3275fbc2623ba79125620600c8c43f
ErwanR-Holberton <6218@holbertonstudents.com>
2024-08-19 17:06:09 +0200

    multiple request

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -7,10 +7,30 @@ if (process.argv[2] == undefined)
 	exit();
 }
 
+async function log_name(url) {
+
+	const options = {
+		url: url,
+		method: 'GET',
+		headers: { 'Content-Type': 'application/json' }
+	};
+
+	request(options, (error, response, body) => {
+		if (error) { console.error('Error making request:', error); return; }
+
+		if (response.statusCode === 200) {
+
+		console.log(JSON.parse(body).name);
+
+		} else { console.error('Failed request. Status code:', response.statusCode); }
+	});
+}
+
+
 const options = {
-  url: 'https://swapi-api.hbtn.io/api/people/',
-  method: 'GET',
-  headers: { 'Content-Type': 'application/json' }
+	url: "https://swapi-api.hbtn.io/api/films/" + process.argv[2],
+	method: 'GET',
+	headers: { 'Content-Type': 'application/json' }
 };
 
 request(options, (error, response, body) => {
@@ -18,11 +38,10 @@ request(options, (error, response, body) => {
 
   if (response.statusCode === 200) {
 
-	const info = JSON.parse(body);
-	info["results"].forEach(element => {
-		if (element.films.includes("https://swapi-api.hbtn.io/api/films/" + process.argv[2] + '/'))
-			console.log(element.name);
-	});
+	JSON.parse(body)["characters"].forEach(element => {
+		options.url = element;
+		log_name(element);
+	});;
 
   } else { console.error('Failed request. Status code:', response.statusCode); }
 });
