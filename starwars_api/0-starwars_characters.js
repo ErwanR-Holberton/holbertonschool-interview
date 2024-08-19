a8cd5ac9031b038680a3258fa287ecf79f47db0b
ErwanR-Holberton <6218@holbertonstudents.com>
2024-08-19 14:49:45 +0200

    Task 0

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,29 @@
+const { Console } = require('console');
+const { exit } = require('process');
+const request = require('request');
+
+if (process.argv[2] == undefined)
+{
+	console.log("Usage: node 0-starwars_characters.js <film id>");
+	exit();
+}
+
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/people/',
+  method: 'GET',
+  headers: { 'Content-Type': 'application/json' }
+};
+
+request(options, (error, response, body) => {
+  if (error) { console.error('Error making request:', error); return; }
+
+  if (response.statusCode === 200) {
+
+	const info = JSON.parse(body);
+	info["results"].forEach(element => {
+		if (element.films.includes("https://swapi-api.hbtn.io/api/films/" + process.argv[2] + '/'))
+			console.log(element.name);
+	});
+
+  } else { console.error('Failed request. Status code:', response.statusCode); }
+});
