a385a7b3ea41a2f3985ed636e075850e9c0cac01
ErwanR-Holberton <124136642+ErwanR-Holberton@users.noreply.github.com>
2024-08-26 13:29:46 +0000

    account for water above walls

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -5,36 +5,15 @@ compute the rain
 def rain(walls):
     if not walls:
         return 0
-    length = len(walls)
-    left = 0
-    while (walls[left] == 0):
-        left += 1
-        if left > length:
-            return 0
-
-    right = left + 1
-    while (walls[right] == 0):
-        right += 1
-        if right > length:
-            return 0
-
-    end = 0
     count = 0
-    while (not end):
-        mini = min(walls[left], walls[right])
-        count += (right - left - 1) * mini
-
-        right += 1
-        if right >= length:
-            return count
-        while (walls[right] == 0):
-            right += 1
-            if right >= length:
-                return count
-        left += 1
-        if left >= length:
-            return count
-        while (walls[left] == 0):
-            left += 1
-            if left >= length:
-                return count
+    for i in range(len(walls)):
+        right = walls[i]
+        left = walls[i]
+        for j in range(i, -1, -1):
+            if walls[j] > left:
+                left = walls[j]
+        for j in range(i, len(walls)):
+            if walls[j] > right:
+                right = walls[j]
+        count += min(left, right) - walls[i]
+    return count
