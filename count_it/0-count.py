3f07bbbc0952d190bc88a4361d35d9640a41dffb
ErwanR-Holberton <6218@holbertonstudents.com>
2024-09-16 16:48:37 +0200

    task 0

---
+++ count_it/0-count.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+count
+"""
+import requests
+from collections import defaultdict
+
+headers = {'User-Agent': 'toto'} 
+def count_words(subreddit, word_list, after=None, word_count=defaultdict(int)):
+    params = {'limit': 100, 'after': after}
+    response = requests.get("https://www.reddit.com/r/{}/hot.json".format(subreddit), headers=headers, params=params)
+    data = response.json()
+    posts = data['data']['children']
+    word_list = [word.lower() for word in word_list]
+    for post in posts:
+        title = post['data']['title'].lower()
+        words_in_title = title.split()
+
+        for word in word_list:
+            word_count[word] += words_in_title.count(word)
+    after = data['data'].get('after')
+    if after:
+        count_words(subreddit, word_list, after=after, word_count=word_count)
+    else:
+        for word, count in sorted(word_count.items(), key=lambda item: (-item[1], item[0])):
+            if count > 0:
+                print(f"{word}: {count}")
+    return word_count
+    
