aa97ae508151cb1d02a27c433296e78b9ef1de80
ErwanR-Holberton <6218@holbertonstudents.com>
2024-09-17 17:26:01 +0200

    pycodestyle

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -4,12 +4,16 @@
 """
 import requests
 
-def count_words(subreddit, word_list, after=None, word_count={}):
-    headers = {'User-Agent': 'toto'} 
-    params = {'limit': 100, 'after': after}
-    response = requests.get("https://www.reddit.com/r/{}/hot.json".format(subreddit), headers=headers, params=params)
 
-    
+def count_words(subreddit, word_list, after=None, wc={}):
+    headers = {'User-Agent': 'toto'}
+    params = {'limit': 100, 'after': after}
+    addr = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(addr, headers=headers, params=params)
+
+    if response.status_code != 200:
+        return
+
     data = response.json()
     posts = data['data']['children']
     word_list = [word.lower() for word in word_list]
@@ -18,11 +22,13 @@ def count_words(subreddit, word_list, after=None, word_count={}):
         words_in_title = title.split()
 
         for word in word_list:
-            word_count[word] =  word_count.get(word, 0) + words_in_title.count(word)
+            wc[word] = wc.get(word, 0) + words_in_title.count(word)
     after = data['data'].get('after')
     if after:
-        count_words(subreddit, word_list, after=after, word_count=word_count)
+        count_words(subreddit, word_list, after=after, wc=wc)
     else:
-        for word, count in sorted(word_count.items(), key=lambda item: (-item[1], item[0])):
+        srt = sorted(wc.items(), key=lambda item: (-item[1], item[0]))
+        for word, count in srt:
             if count > 0:
                 print(f"{word}: {count}")
+    return wc
