748c5dd7fd9098451cb9e77e6d669f180cf62c15
ErwanR-Holberton <124136642+ErwanR-Holberton@users.noreply.github.com>
2024-09-24 19:47:51 +0000

    Task 0 done

--- heap_extract/0-heap_extract.c
+++ heap_extract/0-heap_extract.c
@@ -1,8 +1,73 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
+
+/**
+ * binary_tree_height - function that goes through the tree and mesure height
+ * @tree: tree to loop through
+ *
+ * Return: nothing
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int height = 0, height2 = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	height = binary_tree_height(tree->left);
+	height2 = binary_tree_height(tree->right);
+
+	if (height > height2)
+		return (height + 1);
+	else
+		return (height2 + 1);
+}
+
+/**
+ * heap_extract - remove node root an keep the tree perfect
+ * @root: root of the tree
+ *
+ * Return: value to return
+ */
 int heap_extract(heap_t **root)
 {
-    if (root)
+    int left = 0, right = 0;
+    heap_t *l, *r, *node;
+
+    if (root == NULL)
         return 0;
-    return 1;
+
+    if ((*root)->left == NULL && (*root)->right == NULL)
+    {
+        if ((*root)->parent != NULL)
+        {
+            if ((*root)->parent->left == (*root))
+                (*root)->parent->left = NULL;
+            if ((*root)->parent->right == (*root))
+                (*root)->parent->right = NULL;
+            (*root)->parent = NULL;
+        }
+        right = (*root)->n;
+        free(*root);
+        return (right);
+    }
+
+	left = binary_tree_height((*root)->left);
+	right = binary_tree_height((*root)->right);
+
+    l = (*root)->left;
+    r = (*root)->right;
+
+    if (left > right)
+        node = l;
+    else
+        node = r;
+
+    left = node->n;
+    node->n = (*root)->n;
+    (*root)->n = left;
+
+    return (heap_extract(&node));
 }
