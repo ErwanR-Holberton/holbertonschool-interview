ff1bbf786c2d13ba725eac6032484a7ba4bbf68f
ErwanR-Holberton <6218@holbertonstudents.com>
2024-10-28 16:50:46 +0100

    changed print

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -4,6 +4,24 @@
 #include <string.h>
 #include <stdio.h>
 
+void print_list2(List *list)
+{
+    List *tmp;
+
+    tmp = list;
+    printf("myprint\n");
+    while (tmp)
+    {
+        printf("%s\n", tmp->str);
+        printf("\t->prev: %s\n", tmp->prev ? tmp->prev->str : "NULL");
+        printf("\t->next: %s\n", tmp->next ? tmp->next->str : "NULL");
+        tmp = tmp->next;
+        if (tmp == list)
+            break;
+    }
+    printf("-------\n");
+}
+
 int mylen(List **list)
 {
 	List *start = *list, *temp = start;
@@ -16,7 +34,7 @@ int mylen(List **list)
 		length++;
 		temp = temp->next;
 	}
-	return length;
+	return length +1;
 }
 
 List *add_node_end(List **list, char *str)
@@ -57,6 +75,9 @@ List *add_node_begin(List **list, char *str)
 {
 	int L = mylen(list);
 	if (L == 1)
+	{
 		printf("%d\n", L);
+		print_list2(*list);
+	}
 	return (add_node_end(list, str));
 }
