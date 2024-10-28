1604409b23650b6d01b0fb15792b14fd762fd3a1
ErwanR-Holberton <6218@holbertonstudents.com>
2024-10-28 18:13:11 +0100

    Add betty style and doc

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -5,21 +5,13 @@
 #include <stdio.h>
 
 
-int mylen(List **list)
-{
-	List *start = *list, *temp = start;
-	int length = 0;
-
-	if (start == NULL)
-		return 0;
-	while (temp->next != NULL && temp->next != start)
-	{
-		length++;
-		temp = temp->next;
-	}
-	return length +1;
-}
-
+/**
+ * add_node_end - adds a new node at the end of a circular doubly linked list
+ * @list: double pointer to the head of the list
+ * @str: string to store in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
 List *add_node_end(List **list, char *str)
 {
 	List *new;
@@ -33,7 +25,10 @@ List *add_node_end(List **list, char *str)
 
 	new->str = strdup(str);
 	if (new->str == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 
 	new->prev = new;
 	new->next = new;
@@ -47,16 +42,27 @@ List *add_node_end(List **list, char *str)
 	{
 		if ((*list)->prev != NULL)
 			(*list)->prev->next = new;
+
 		new->next = *list;
 		new->prev = (*list)->prev;
 		(*list)->prev = new;
+
 		return (new);
 	}
-	return NULL;
+	return (NULL);
 }
+
+/**
+ * add_node_begin - adds node at the beginning of a circular doubly linked list
+ * @list: double pointer to the head of the list
+ * @str: string to store in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
 List *add_node_begin(List **list, char *str)
 {
 	List *new;
+
 	new = add_node_end(list, str);
 	*list = new;
 	return (new);
