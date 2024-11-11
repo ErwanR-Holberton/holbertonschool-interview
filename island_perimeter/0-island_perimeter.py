2e2ccd7a3f4f6f477a15e07e8ba7fed129df0eb4
ErwanR-Holberton <6218@holbertonstudents.com>
2024-11-11 10:54:13 +0100

    Fix shebang

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,10 +1,16 @@
+#!/usr/bin/python3
+"""find perimeter of island"""
+
+
 def safe_get_2d(array, row, col, default=0):
+    """get 1 if water 0 for land ignore out of bounds"""
     try:
         return 1 - array[row][col]
     except IndexError:
         return default
 
 def island_perimeter(grid):
+    """find perimeter of island"""
     perimeter = 0
     for x in range(len(grid)):
         for y in range(len(grid[0])):
