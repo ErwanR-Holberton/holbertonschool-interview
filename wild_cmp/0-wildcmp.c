6945c6355da0ce7bb058d06936cc6b9333873396
ErwanR-Holberton <6218@holbertonstudents.com>
2024-11-25 11:30:10 +0100

    Betty style

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -19,5 +19,5 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(&s1[1], &s2[1]));
 	if (s2[0] == '*')
 		return (wildcmp(&s1[1], &s2[0]) || wildcmp(&s1[0], &s2[1]));
-	return 0;
+	return (0);
 }
